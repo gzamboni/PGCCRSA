37be6557c2c1f6fd2a3a6a59298be54ca5588e96
gzamboni <gzamboni@gmail.com>
2013-10-19 15:13:06 -0300

    testes e teste

--- src/PGCCRSA.java
+++ src/PGCCRSA.java
@@ -5,12 +5,12 @@ import utils.*;
 public class PGCCRSA {
 
 	public static void main(String[] args) {
-		RSA rsaAlg = new RSA(BigInteger.valueOf(13),BigInteger.valueOf(11));
+		RSA rsaAlg = new RSA(9);
 		ChaveRSA chave = rsaAlg.getChaveRSA();
 		System.out.printf("Chave privada (d,n) = (%s,%s):\n", chave.getD().toString(), chave.getN().toString());
 		System.out.printf("Chave publica (e,n) = (%s,%s):\n", chave.getE().toString(), chave.getN().toString());
 		
-		BigInteger mensagem = new BigInteger("5");
+    	BigInteger mensagem = new BigInteger("69809");
 		BigInteger cripto = new BigInteger("0");
 		
 		cripto = mensagem.modPow(chave.getE(), chave.getN());
@@ -27,5 +27,9 @@ public class PGCCRSA {
 		
 		System.out.println(args[0]);
 		System.out.println(args[1]);
+
+		forcaBruta forte = new forcaBruta(chave.getN());
+		System.out.println("Original " + rsaAlg.getP().toString() + "," + rsaAlg.getQ().toString());
+		System.out.println("Quebrado " + forte.getP().toString() + "," + forte.getQ().toString());
 	}
 }
