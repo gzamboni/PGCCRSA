1d478c829a19147f988f43f64fd6c8325df3a9f2
gzamboni <gzamboni@gmail.com>
2013-10-19 10:44:54 -0300

    Versão Inicial

---
+++ src/PGCCRSA.java
@@ -0,0 +1,11 @@
+import utils.Primos;
+
+@SuppressWarnings("unused")
+public class PGCCRSA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/utils/Primos.java
@@ -0,0 +1,42 @@
+package utils;
+
+import java.math.*;
+import java.util.Random;
+
+public class Primos {
+
+	private final static Random rand = new Random();
+	
+    private static BigInteger getRandomFermatBase(BigInteger n)
+    {
+        // Rejection method: ask for a random integer but reject it if it isn't
+        // in the acceptable set.
+
+        while (true)
+        {
+            final BigInteger a = new BigInteger (n.bitLength(), rand);
+            // must have 1 <= a < n
+            if (BigInteger.ONE.compareTo(a) <= 0 && a.compareTo(n) < 0)
+            {
+                return a;
+            }
+        }
+    }
+	
+	public static boolean checkPrime(BigInteger n, int maxIterations)
+    {
+        if (n.equals(BigInteger.ONE))
+            return false;
+
+        for (int i = 0; i < maxIterations; i++)
+        {
+            BigInteger a = getRandomFermatBase(n);
+            a = a.modPow(n.subtract(BigInteger.ONE), n);
+
+            if (!a.equals(BigInteger.ONE))
+                return false;
+        }
+
+        return true;
+    }
+}
